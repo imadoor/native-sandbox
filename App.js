41fcf1b68fa375d6ba0a03568d1fb9c1360b754a
Amador Ollarzabal <amador.ollarzabal@outlook.com>
2021-10-12 22:46:52 -0500

    initial setup

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState('Before');
+  const [person, setPerson] = useState({ name: 'First', age: 30 });
+  const [counter, setCounter] = useState(0)
+  const clickUpdateHandler = ()=>{
+    // setName('After');
+    // setPerson({ name: 'Second', age: 40 })
+    setCounter(counter + 1);
+  }
+  const clickDefaultHandler = ()=>{
+    setName('Name');
+    setPerson({ name: 'First', age: 30 })
+    setCounter(0);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>{counter}</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>{name}</Text>
+        <TextInput 
+          style={styles.input}
+          placeholder='Full Name'
+          onChangeText={(value)=>{
+            setName(value)
+          }}
+        />
+      </View>
+      
+      <View style={styles.buttonContainer}>
+        <Button title='update state' onPress={clickUpdateHandler} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title='default state' onPress={clickDefaultHandler} />
+      </View>
     </View>
   );
 }
@@ -18,4 +49,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText:{
+    fontWeight: 'bold',
+  },
+  body:{
+    backgroundColor: 'yellow',
+    padding: 20,
+  }, 
+  buttonContainer: {
+    marginTop: 20,
+  },
+  input:{
+    borderWidth: 1, 
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+  } 
 });
